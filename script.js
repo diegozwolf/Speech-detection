ecc541bc95170d6df6d42ca84fba071ac280a725
diegozwolf <diegozuluaga88@gmail.com>
2017-03-01 16:34:06 -0500

    comments in script js

--- script.js
+++ script.js
@@ -4,17 +4,19 @@
   const recognition = new SpeechRecognition();
   recognition.interimResults = true;
 
+  //permite crear los textos
   let p = document.createElement('p');
+  //guarda los textos que son creados
   const words = document.querySelector('.words');
   words.appendChild(p);
-
+  //función para guardar voz en un array
   recognition.addEventListener('result', e => {
     console.log(e.results);
     const transcript = Array.from(e.results)
       .map(result => result[0])
       .map(result => result.transcript)
       .join('');
-      
+      //función para reconocer voz a texto
       p.textContent = transcript;      
       if (e.results[0].isFinal) {
         p = document.createElement('p');
